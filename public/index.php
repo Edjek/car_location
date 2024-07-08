fef9148fc321de89f84285fd9dc01bd1713700b9
Edjek <edjek@hotmail.fr>
2024-07-08 10:34:48 +0200

    create class Autoloader

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
 
-require_once '../src/Entity/Test.php';
+require_once '../src/Core/Autoloader.php';
 
 use App\Entity\Test;
+use App\Core\Autoloader;
+
+Autoloader::autoload();
 
 $test = new Test();
 echo $test->display();
